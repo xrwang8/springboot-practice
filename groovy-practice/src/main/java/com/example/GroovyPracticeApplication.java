d82c01e23235a4879ebf611dd0ab64cd77a4b0c7
xrwang8 <68765051+xrwang8@users.noreply.github.com>
2021-06-24 21:03:41 +0800

    初始化提交

---
+++ groovy-practice/src/main/java/com/example/GroovyPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovyPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroovyPracticeApplication.class, args);
+    }
+
+}
