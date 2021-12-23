6af3300c6d4834683f7f7ab1dc2d55ae3d721005
xrwang8 <68765051+xrwang8@users.noreply.github.com>
2021-12-23 16:47:07 +0800

    feat: 新增多线程联系模块

---
+++ thread-practice/src/main/java/com/example/thread/ThreadPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadPracticeApplication.class, args);
+    }
+
+}
